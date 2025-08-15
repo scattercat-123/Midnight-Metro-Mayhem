73bff101f706d3e8fd8f1c29590344c65543fced
Atharv Sharma <replyatharv@gmail.com>
2025-08-15 21:52:33 +0530

    made small changes fo rhtml

--- assets/scripts/start.gd
+++ assets/scripts/start.gd
@@ -1,15 +1,20 @@
 extends Sprite2D
 
 @export var start_without_animation: bool = false
-## this is for debuggging
+@export var animation_duration: float = 10.0  # seconds
 
 func _ready():
 	if start_without_animation:
 		get_tree().change_scene_to_file("res://assets/scenes/level1.tscn")
+	else:
+		# Wait for the animation to finish
+		await get_tree().create_timer(animation_duration).timeout
+		get_tree().change_scene_to_file("res://assets/scenes/level1.tscn")
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		var local_pos = to_local(event.position)
-		if Rect2(Vector2.ZERO, texture.get_size()).has_point(local_pos):
-			print("Going to level1")
-			get_tree().change_scene_to_file("res://assets/scenes/level1.tscn")
+#func _input(event):
+	#if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		#var local_pos = to_local(event.position)
+		#if Rect2(Vector2.ZERO, texture.get_size()).has_point(local_pos):
+			#print("Going to level1")
+			#get_tree().change_scene_to_file("res://assets/scenes/level1.tscn")
+			
